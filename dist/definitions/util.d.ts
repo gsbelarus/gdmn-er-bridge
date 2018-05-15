5cec4f91d021719a8e3eaca2c2a164cdb9093234
andreik <gs1994@gmail.com>
2018-05-15 20:09:59 +0300

    wip

---
+++ dist/definitions/util.d.ts
@@ -0,0 +1,4 @@
+import { ContextVariables } from 'gdmn-orm';
+export declare function default2Int(defaultValue: string | null): number | undefined;
+export declare function default2Number(defaultValue: string | null): number | undefined;
+export declare function default2Date(defaultValue: string | null): Date | ContextVariables | undefined;
